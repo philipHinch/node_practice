7d9ddd5945f40c68acc6d6b2abf6ba7b0f701862
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2022-03-27 11:04:05 +0300

    simple html page routing

--- server.js
+++ server.js
@@ -7,8 +7,24 @@ const server = http.createServer((req, res) => {
     //set header content type
     res.setHeader('Content-Type', 'text/html');
 
+    //simple routing
+
+    let path = './views/';
+
+    switch (req.url) {
+        case '/':
+            path += 'index.html'
+            break;
+        case '/about':
+            path += 'about.html'
+            break;
+        default:
+            path += '404.html'
+            break;
+    }
+
     //send html file
-    fs.readFile('./views/index.html', (err, data) => {
+    fs.readFile(path, (err, data) => {
         if (err) {
             console.log(err);
             res.end()
