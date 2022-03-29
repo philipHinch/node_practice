7d2ea115322039a7bc41bb99e81dac1a03430fe9
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2022-03-29 21:40:52 +0300

    ejs and dynamic data

--- app.js
+++ app.js
@@ -13,28 +13,24 @@ app.set('view engine', 'ejs')
 app.listen(3000) //if we dont specify the second argument, it automatically refers to localhost
 
 app.get('/', (req, res) => {
-    //send method automatically figuers out the type of response, so there is no need to specify the type in the headers like:res.setHeader('Content-Type', 'text/html');
-
-    //res.send('<h1>home page</h1>')
-
-    //send html page
-    res.sendFile('./views/index.html', { root: __dirname }) //specify the root directory (what is the path relative to?)
+    //send ejs page
+    //with ejs, just specify the name of the file in the views folder. no need for path and root object. now we "render" the ejs file
+    //send dynamic values to page inside the second paramenter object
+    res.render('index', { title: 'Home' })
 })
 
 //send html page
 app.get('/about', (req, res) => {
-    //res.send('<h1>about page</h1>')
-    res.sendFile('./views/about.html', { root: __dirname })
+    res.render('about', { title: 'About' })
 })
 
-//redirect
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create' })
 })
 
 //404
 app.use((req, res) => {
     //middleware function, must be at the end of the code and is triggered if none of the above responses are sent
-    res.status(404).sendFile('./views/404.html', { root: __dirname }) //specify also the status code
+    res.status(404).render('404', { title: '404' }) //specify also the status code
 
 }) 
